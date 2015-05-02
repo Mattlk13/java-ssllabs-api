902d1fb48757646d4237001a2f94b47747943654
bjoernr-de <bjoern.roland@gmail.com>
2015-05-02 23:59:18 +0200

    Added host information CLI support

--- src/de/bjoernr/ssllabs/Console.java
+++ src/de/bjoernr/ssllabs/Console.java
@@ -1,9 +1,12 @@
 package de.bjoernr.ssllabs;
 
+import de.bjoernr.ssllabs.Api;
 import de.bjoernr.ssllabs.ConsoleUtilities;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.json.JSONObject;
 
@@ -21,6 +24,10 @@ public class Console {
 		if(args.length == 1 && (args[0].equals("--info") || args[0].equals("-i")))
 		{
 			handleInfo();
+		}
+		else if((args.length > 0 && args.length <= 6) && (args[0].equals("--host-information") || args[0].equals("-hi")))
+		{
+			handleHostInformation(args);
 		}
 		else
 		{
@@ -40,6 +47,65 @@ public class Console {
 		System.out.println(ConsoleUtilities.mapToConsoleOutput(map));
 	}
 	
+	public static void handleHostInformation(String[] args)
+	{	
+		//API parameters
+		String host = arrayValueMatchRegex(args, "-h=(.+)");
+		boolean publish = false;
+		boolean startNew = false;
+		boolean fromCache = false;
+		String maxAge = null;
+		String all = null;
+		boolean ignoreMismatch = false;
+		
+		if(host == null)
+		{
+			//host not found in arguments
+			printUsage();
+			return;
+		}
+		
+		String[] possibleArguments = {"-p", "-c", "-m", "-a", "-i"};
+		
+		for(String arg : possibleArguments)
+		{
+			if(arrayValueMatchRegex(args, arg) == null)
+			{
+				//if argument is not in args array, continue with next possible argument
+				continue;
+			}
+			
+			switch(arg)
+			{
+			case "-p":
+				publish = true;
+				break;
+			case "-c":
+				fromCache = true;
+				break;
+			case "-m":
+				maxAge = arrayValueMatchRegex(args, "-m=(.+)");
+				break;
+			case "-a":
+				all = arrayValueMatchRegex(args, "-a=(.+)");
+				break;
+			case "-i":
+				ignoreMismatch = true;
+				break;
+			}
+		}
+		
+		Api ssllabsApi = new Api();
+		
+		JSONObject hostInformation = ssllabsApi.fetchHostInformation(host, publish, startNew, fromCache, maxAge, all, ignoreMismatch);
+		
+		Map<String, Object> map = ConsoleUtilities.jsonToMap(hostInformation);
+		
+		System.out.println("Host information");
+		System.out.println("");
+		System.out.println(ConsoleUtilities.mapToConsoleOutput(map));
+	}
+	
 	public static void printHeader()
 	{
 		System.out.println("");
@@ -49,7 +115,7 @@ public class Console {
 		System.out.println("    | |/ _` \\ \\ / / _` |  `--. \\`--. \\ |    | |    / _` | '_ \\/ __| |  _  ||  __/  | |  ");
 		System.out.println("/\\__/ / (_| |\\ V / (_| | /\\__/ /\\__/ / |____| |___| (_| | |_) \\__ \\ | | | || |    _| |_ ");
 		System.out.println("\\____/ \\__,_| \\_/ \\__,_| \\____/\\____/\\_____/\\_____/\\__,_|_.__/|___/ \\_| |_/\\_|    \\___/ ");
-		System.out.println("by Björn Roland <https://github.com/bjoernr-de>");
+		System.out.println("by Bjoern Roland <https://github.com/bjoernr-de>");
 		System.out.println("and contributors (https://github.com/bjoernr-de/java-ssllabs-api/graphs/contributors)");
 		System.out.println("-------------------------------------------------");
 		System.out.println("");
@@ -66,5 +132,43 @@ public class Console {
 		
 		System.out.println("-i, --info");
 		System.out.println("	Fetch API information");
+		System.out.println("");
+		System.out.println("-hi, --host-information");
+		System.out.println("	Mandatory parameter:");
+		System.out.println("	-h, --host (String)");
+		System.out.println("");
+		System.out.println("	Additional parameter:");
+		System.out.println("	-p, --publish (boolean) - default value is false");
+		System.out.println("	-c, --fromCache (boolean) - default value is false");
+		System.out.println("	-m, --maxAge (String)");
+		System.out.println("	-a, --all (String)");
+		System.out.println("	-i, --ignoreMismatch (boolean) - default value is false");
+		System.out.println("");
+		System.out.println("	Example:");
+		System.out.println("	" + jarExecution + " -hi -h=https://ssllabs.com -p -c -m=\"1\"");
+	}
+	
+	public static String arrayValueMatchRegex(String[] array, String regex)
+	{
+		Pattern p = Pattern.compile(regex);
+		
+		for(int i=0; i<array.length; i++)
+		{
+			Matcher m = p.matcher(array[i]);
+			
+			while(m.find())
+			{
+				try
+				{
+					return(m.group(1));
+				}
+				catch (Exception ignored)
+				{
+					//possible IndexOutOfBoundsException
+				}
+ 			}
+		}
+		
+		return null;
 	}
 }
